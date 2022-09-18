04f2d6c9bd02d0e2268f95aed0b982c7c798d8d2
Yogesh-G-L <113408088+Yogesh-G-L@users.noreply.github.com>
2022-09-18 22:40:09 +0530

    Add files via upload

---
+++ distric.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+class distric  extends city{
+	
+	public void ndistric() {
+		System.out.println("bengaluru urban");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		distric s=new distric();
+		s.nstate();
+		s.ncity();
+		s.ndistric();
+
+	}
+}
