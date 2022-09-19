7bcb47fa790838557b604193a0817fd8c89ccf47
Yogesh-G-L <113408088+Yogesh-G-L@users.noreply.github.com>
2022-09-19 10:07:21 +0530

    assignment 2
    assignment of inheritance

---
+++ mouse2.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class mouse2 extends mouse{
+	
+	public static void  connected () {
+		System.out.println("Connected  ");
+			
+	}
+
+}
---
+++ mouse3.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class mouse3 extends mouse  {
+	public static void design() {
+		System.out.println(" new one ");
+		
+	}
+
+}
