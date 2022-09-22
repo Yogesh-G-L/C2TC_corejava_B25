bc2fc0fcdb35e671811dde25dd9cc132f265e0ef
Yogesh-G-L <113408088+Yogesh-G-L@users.noreply.github.com>
2022-09-22 22:03:02 +0530

    Update prime_member.java

--- interface_assignment3/src/com/tns/interface_assignment3/prime_member.java
+++ interface_assignment3/src/com/tns/interface_assignment3/prime_member.java
@@ -5,7 +5,7 @@ public class prime_member implements interface_class {
 	@Override 
 	public void delivary() {// calling the interface method. 
 		
-		System.out.println(" Delivary charges for this account"+0);
+		System.out.println(" Delivary charges for this account = "+0);
 		// TODO Auto-generated method stub
 		
 	}
